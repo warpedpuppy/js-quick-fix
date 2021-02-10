07719d795cf6b6c102d1cf2f128d6716726f0070
jingruS <36281068+jingruS@users.noreply.github.com>
2021-02-09 20:10:12 -0800

    first PokemonList jsFile

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-alert('Hello World');
-let favoriteFood ='Pizza';
-document.write(favoriteFood);
+let PokemonList= [
+  {Pokemon 1},
+  {Pokemon 2},
+  {Pokemon 3}
+]
+let Pokemon 1= {
+  name:'Pikachu',
+  height:1'04",
+  types:['electric']
+};
+
+let Pokemon 2= {
+  name:'Clefairy',
+  height:2'00",
+  types:['fairy']
+};
+
+let Pokemon 3= {
+  name:'Jigglypuff',
+  height:1'08",
+  types:[
+    'normal',
+    'fairy'
+  ]
+};
