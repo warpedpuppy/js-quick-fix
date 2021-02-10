810dbdd0fdd5fd2db1092d4212fb951b96c3050b
jingruS <36281068+jingruS@users.noreply.github.com>
2021-02-10 13:00:19 -0800

    undated

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,6 @@ let PokemonList = [
     {name:'Clefairy',height:1.1,types:['fairy']},
     {name:'Jigglypuff',height:1.08,types:['normal','fairy']}
   ]
+  for (let i=0; i < PokemonList.length; i++){
+    console.log(PokemonList[i].name)
+  }
