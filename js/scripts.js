37c7da4cf70b8c4e23175cde78ecd53329382732
jingruS <36281068+jingruS@users.noreply.github.com>
2021-02-10 10:59:14 -0800

    wirth no inches sign

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
-let PokemonList= [
-  {Pokemon 1},
-  {Pokemon 2},
-  {Pokemon 3}
-]
-let Pokemon 1= {
-  name:'Pikachu',
-  height:1'04",
-  types:['electric']
-};
-
-let Pokemon 2= {
-  name:'Clefairy',
-  height:2'00",
-  types:['fairy']
-};
-
-let Pokemon 3= {
-  name:'Jigglypuff',
-  height:1'08",
-  types:[
-    'normal',
-    'fairy'
-  ]
-};
+let PokemonList = [{name:'Pikachu',height:0.5, types:['electric']},
+ {name: 'Pikachu', height:0.6,  types:['electric']},
+ {name: 'Jigglypuff',height:0.5,types:['normal','fairy']}
+];
