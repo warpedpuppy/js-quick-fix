66ddce531b7cfa8322a273f65267d0792e4bd420
jingruS <36281068+jingruS@users.noreply.github.com>
2021-02-10 16:44:11 -0800

    condition added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 let PokemonList = [
     {name:'Pikachu',height:0.9,types:['electric']},
-    {name:'Clefairy',height:1.1,types:['fairy']},
-    {name:'Jigglypuff',height:1.08,types:['normal','fairy']}
+    {name:'Clefairy',height:0.5,types:['fairy']},
+    {name:'Jigglypuff',height:1.8,types:['normal','fairy']}
   ]
   for (let i=0; i < PokemonList.length; i++){
-    console.log(PokemonList[i].name)
-  }
+
+    if(PokemonList[i].height > 1){
+    console.log (`Name: ${PokemonList[i].name} (height: ${PokemonList[i].height})
+    'Oh! It is a big one!'`)
+    }else{
+      console.log(`Name: ${PokemonList[i].name} (height: ${PokemonList[i].height})`)
+    }
+
+     document.write (`Name: ${PokemonList[i].name} (height: ${PokemonList[i].height}) `)
+    }
