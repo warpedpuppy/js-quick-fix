7e91ca1a745a272d5c4b1b1e5534acc4334853f8
jingruS <36281068+jingruS@users.noreply.github.com>
2021-03-22 09:45:29 -0700

    Include under IIFE
    include PokemonList into IIFE and revised loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
-let PokemonList = [
+let PokemonRepository =(function () {
+
+    let PokemonList = [
     {name:'Pikachu',height:0.9,types:['Electric']},
     {name:'Clefairy',height:0.5,types:['Fairy']},
-    {name:'Jigglypuff',height:1.8,types:['Normal',' Fairy']}
+    {name:'Jigglypuff',height:1.8,types:['Normal','Fairy']}
   ];
 
-  (function () {
-      PokemonList.forEach(function(display) {
+    function add(display) {
+        PokemonList.push(display);
+      }
+    function getAll () {
+       return PokemonList;
+      }
+    return {
+        add:add,
+        getAll:getAll
+    };
+  }) ();
+
+  PokemonRepository.add ({ name:'Cledairy', height:2 , types: ['Fairy']});
+  console.log(PokemonRepository.getAll());
+
+
+(function (){
+      PokemonRepository.getAll().forEach(function(display) {
 
         if (display.height > 1 ) {
-           document.write( display.name + ' ' + 'Height: ' + display.height + ' ' + 'Type:'+ display.types + ' ' + 'Woo, that \'s a big one!' + '<p> </p>');
+           document.write( display.name + ' ' + 'Height: ' + display.height + ' ' + display.types + ' ' + '-Woo, that \'s a big one!' + '<p> </p>');
       } else {
-          document.write(display.name + ' ' + 'Height: ' + display.height + ' ' + 'Type:'+ display.types + ' ' + '<p> </p>');
+          document.write(display.name + ' ' + 'Height: ' + display.height + ' ' + display.types + ' ' + '<p> </p>');
       }});
-  })();
+
+ })();
