953a4f25fd7b2009f03ca77c4fe9c7fc30c1090b
jingruS <36281068+jingruS@users.noreply.github.com>
2021-03-24 16:05:55 -0700

    Basic DOM

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,49 @@ let PokemonRepository =(function () {
 
     function add(display) {
         PokemonList.push(display);
-      }
+        }
     function getAll () {
        return PokemonList;
       }
+
+      // creating lists and button in the DOM
+    function addListItem(display){
+        let pokemonList= document.querySelector ('.pokemon-list');
+        let listPokemon = document.createElement ('li');
+        let button = document.createElement('button');
+        button.innerText= display.name;
+        button.classList.add('button-class')
+        listPokemon.appendChild(button);
+        pokemonList.appendChild (listPokemon);
+        //Event listner on click
+        button.addEventListener ('click',function (event) {
+          showDetails(display);
+           });
+    }
+
+    function showDetails (display){
+      let text='Woo! It is big!'
+      if (display.height>1){
+        console.log(display);
+        console.log(text);
+      }else{
+       console.log (display);
+       }
+    }
+
     return {
         add:add,
-        getAll:getAll
+        getAll:getAll,
+        addListItem:addListItem
     };
+
   }) ();
 
   PokemonRepository.add ({ name:'Cledairy', height:2 , types: ['Fairy']});
-  console.log(PokemonRepository.getAll());
 
 
-(function (){
-      PokemonRepository.getAll().forEach(function(display) {
 
-        if (display.height > 1 ) {
-           document.write( display.name + ' ' + 'Height: ' + display.height + ' ' + display.types + ' ' + '-Woo, that \'s a big one!' + '<p> </p>');
-      } else {
-          document.write(display.name + ' ' + 'Height: ' + display.height + ' ' + display.types + ' ' + '<p> </p>');
-      }});
+  PokemonRepository.getAll().forEach(function(display){
 
- })();
+  PokemonRepository.addListItem(display);
+    });
